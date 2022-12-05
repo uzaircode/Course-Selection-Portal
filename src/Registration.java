503e7d95d11d86ab278189403f9c9296c4b13a75
Nik Uzair <nikuzairsc@gmail.com>
2022-12-05 08:41:37 +0800

    Update Registration.java
    - Remove redundant code.

--- src/Registration.java
+++ src/Registration.java
@@ -4,7 +4,7 @@ public class Registration {
 
     String username;
     String password;
-    // Student stud1 = new Student(username, password, 1201200657);
+    Student stud1 = new Student(username, password, 1201200657, MediumStudy.UNDERGRADUATE);
 
     public void displayLogin() {
         Scanner input = new Scanner(System.in);
@@ -12,9 +12,10 @@ public class Registration {
         System.out.println("===== LOGIN =====");
         System.out.print("Enter username: ");
         username = input.nextLine();
+        stud1.setUsername(username);
         System.out.print("Enter password: ");
         password = input.nextLine();
-
+        stud1.setPassword(password);
     }
 
     // public void displayRegister() {
@@ -37,22 +38,18 @@ public class Registration {
         Course res = new Course(password, password, 0, 0, MediumStudy.FOUNDATION);
 
         System.out.print("\033[H\033[2J");
+        System.out.println("===== STUDENT INFORMATION");
+        System.out.println("Student Name : " + stud1.getUsername());
+        System.out.println("Password     : " + stud1.getPassword());
+        System.out.println("Student ID   : " + stud1.getStudentId() + "\n\n");
+
         System.out.println("===== DASHBOARD =====");
-        // System.out.println("Welcome " + stud1.getUsername() + "!");
-        // System.out.println("Student Id: " + stud1.getStudentId());
-        System.out.println("\n1. STUDENT INFORMATION");
-        System.out.println("2. COURSE ENROLLMENT");
-        System.out.print("\n Choose 1 : ");
+        System.out.println("Welcome " + stud1.getUsername() + "!");
+        System.out.println("\n1. COURSE ENROLLMENT");
+        System.out.print("\nChoose 1 : ");
         int choice = input.nextInt();
 
-        if (choice == 1) {
-            System.out.print("\033[H\033[2J");
-            System.out.println("===== STUDENT INFORMATION");
-            // System.out.println("Student Name : " + stud1.getUsername());
-            // System.out.println("Password : " + stud1.getPassword());
-            // System.out.println("Student ID : " + stud1.getStudentId());
-        } else if (choice == 2) {
+        if (choice == 1)
             res.displayCourseList();
-        }
     }
 }
