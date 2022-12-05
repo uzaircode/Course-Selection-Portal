2f4318083d94ade2cf0e0537ed24d35b5ab18858
Nik Uzair <nikuzairsc@gmail.com>
2022-12-05 10:10:24 +0800

    Update Main.java
    - Delete login & register code.
    1. Mainly focus on system rather than the user interface.

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,6 @@ public class Main {
         System.out.print("\033[H\033[2J");
         Registration res = new Registration();
 
-        int choice;
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("=====MAIN MENU=====");
-        System.out.println("1. Login");
-        System.out.println("2. Register");
-
-        System.out.print("Pick 1 : ");
-        choice = input.nextInt();
-
-        if (choice == 1) {
-            System.out.print("\033[H\033[2J");
-            res.displayLogin();
-            res.menu();
-        } else if (choice == 2) {
-            System.out.print("\033[H\033[2J");
-            // res.displayRegister();
-            res.menu();
-        }
+        res.menu();
     }
 }
