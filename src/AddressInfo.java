fe600ed32607824b9844e4ad13438d08c2f428e6
Nik Uzair <nikuzairsc@gmail.com>
2023-01-14 10:34:59 +0800

    Create AddressInfo.java
    - Add attributes, constructor, getter & setter, toString method.
    - AddressInfo is part of composition.

---
+++ src/AddressInfo.java
@@ -0,0 +1,69 @@
+public class AddressInfo {
+    private String street;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String country;
+
+    // AddressInfo is Part of composition.
+    // Definition : Composition Like Aggregation but the owned object is not
+    // independent from the owner which means by deleting owner the owned object
+    // will also be deleted.
+    public AddressInfo(String street, String city, String state, String postalCode, String country) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.postalCode = postalCode;
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "HomeAddress [street=" + street + ", city=" + city + ", state=" + state + ", postalCode=" + postalCode
+                + ", country=" + country + "]";
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void validate() {
+    }
+
+}
