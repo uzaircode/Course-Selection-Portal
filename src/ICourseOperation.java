ac92d6fc30b7475c9217ea6d4de84e20f7af9bc8
Nik Uzair <nikuzairsc@gmail.com>
2023-01-20 08:47:14 +0800

    Update ICourseOperation
    - Add manageCourse().

--- src/ICourseOperation.java
+++ src/ICourseOperation.java
@@ -1,4 +1,7 @@
 interface ICourseOperation {
+
+    void manageCourse(Course selectedCourse, User loggedInUser);
+
     void addCourse(Course selectedCourse, User loggedInUser);
 
     void unregisteredFromCourse(Course course, User loggedInUser);
