9ee0dee4697ee939bff6af174db8e324d710096f
Nik Uzair <nikuzairsc@gmail.com>
2023-01-20 10:46:49 +0800

    Update Admin.java
    - Add AdminOperation(), and implement addCourse() inside it.
    - Implement design pattern.
    - Create new attribute.

--- src/Admin.java
+++ src/Admin.java
@@ -1,38 +1,116 @@
-import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Admin extends User implements Dashboard, ICourseOperation {
     private String adminId;
+    private AdminOperation adminOperation;
+
+    public Admin() {
+        this.adminOperation = new AdminOperation();
+    }
 
     public Admin(String username, String password, String emailAddress, int phoneNumber, AddressInfo addressInfo,
             String adminId) {
         super(username, password, emailAddress, phoneNumber, addressInfo);
         this.adminId = adminId;
+        this.adminOperation = new AdminOperation();
     }
 
     public Admin(String username, String password) {
         super(username, password);
-    }
-
-    public Admin() {
+        this.adminOperation = new AdminOperation();
     }
 
     public String getAdminId() {
         return adminId;
     }
 
-    // @Override
-    // public void displayDashboard(User loggedInUser) {
-    // System.out.print("\033[H\033[2J");
-    // System.out.println("Hello");
-    // }
+    public void displayAddCourses(Course selectedCourse, User loggedInUser) {
+        adminOperation.execute(selectedCourse, loggedInUser);
+    }
 
-    // @Override
-    // public void addCourse() {
-    // System.out.println("admin add course is working...");
-    // }
+    private class AdminOperation extends CourseOperation {
+        @Override
+        public void addCourse(Course selectedCourse, User loggedInUser) {
+            System.out.print("\033[H\033[2J");
+            try (Scanner input = new Scanner(System.in)) {
+                System.out.println("===== CREATE NEW COURSE =====\n");
+                System.out.print("Enter course name : ");
+                String courseName = input.nextLine();
+
+                // System.out.print("Enter course description: ");
+                // String courseDescription = input.nextLine();
+                // System.out.println("Enter the number of programme : ");
+                // int arraySize = input.nextInt();
+
+                // String[] programme = new String[arraySize];
+
+                // System.out.println("Enter the name of the programme : ");
+                // for (int i = 0; i < arraySize; i++) {
+                // programme[i] = input.next();
+                // }
+
+                // System.out.print("Enter course duration : ");
+                // int courseDuration = input.nextInt();
+
+                // String[] dd = new String[arraySize];
+
+                // System.out.println("Enter the name of the programme : ");
+                // for (int i = 0; i < arraySize; i++) {
+                // dd[i] = input.next();
+                // }
+
+                // String[] ee = new String[arraySize];
+
+                // System.out.println("Enter the name of the programme : ");
+                // for (int i = 0; i < arraySize; i++) {
+                // ee[i] = input.next();
+                // }
+
+                // System.out.print("Enter Fee Structure : ");
+                // int feeStructure = input.nextInt();
+
+                // System.out.print("Enter maximum student : ");
+                // int maximumStudent = input.nextInt();
+
+                // System.out.println("Scholarship Facilities : ");
+                // boolean scholarshipFacilities = input.nextBoolean();
+
+                // Course c = new Course("2121212", courseName, programme, courseDuration, dd,
+                // ee, true, feeStructure,
+                // maximumStudent, MediumOfStudy.FOUNDATION);
+                // Course science = new Course("ODN3829", "Bachelor of Science (Hons.)
+                // Intelligent Robotics",
+                // new String[] { "Data Structures", "Algorithms", "Programming Languages" },
+                // 120,
+                // new String[] { "Masters in Computer Science",
+                // "MBA in IT" },
+                // new String[] { "hello" },
+                // true, 14, 120, MediumOfStudy.FOUNDATION);
+
+                Course c = new Course(courseName);
+                Course.addCourse(c);
+
+                List<Course> allCourses = Course.getAllCourses();
+
+                for (Course course : allCourses) {
+                    System.out.println(course.getCourseName());
+                }
+                System.out.println("\nCourse " + c.getCourseName() + " has been created successfdeully.");
+
+                System.out.print("\nPress 0 to return : ");
+                int selection = input.nextInt();
+                if (selection == 0)
+                    manageCourse(c, loggedInUser);
+            } catch (
+
+            NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 
     @Override
     void displayInformation(User loggedInUser) {
@@ -58,87 +136,6 @@ public class Admin extends User implements Dashboard, ICourseOperation {
             int selection = input.nextInt();
             if (selection == 0)
                 res.studentDashboard(loggedInUser);
-        }
-    }
-
-    @Override
-    public void addCourse(Course selectedCourse, User loggedInUser) {
-        System.out.print("\033[H\033[2J");
-        try (Scanner input = new Scanner(System.in)) {
-
-            System.out.println("===== CREATE NEW COURSE =====\n");
-
-            System.out.print("Enter course name : ");
-            String courseName = input.nextLine();
-
-            // System.out.print("Enter course description: ");
-            // String courseDescription = input.nextLine();
-            // System.out.println("Enter the number of programme : ");
-            // int arraySize = input.nextInt();
-
-            // String[] programme = new String[arraySize];
-
-            // System.out.println("Enter the name of the programme : ");
-            // for (int i = 0; i < arraySize; i++) {
-            // programme[i] = input.next();
-            // }
-
-            // System.out.print("Enter course duration : ");
-            // int courseDuration = input.nextInt();
-
-            // String[] dd = new String[arraySize];
-
-            // System.out.println("Enter the name of the programme : ");
-            // for (int i = 0; i < arraySize; i++) {
-            // dd[i] = input.next();
-            // }
-
-            // String[] ee = new String[arraySize];
-
-            // System.out.println("Enter the name of the programme : ");
-            // for (int i = 0; i < arraySize; i++) {
-            // ee[i] = input.next();
-            // }
-
-            // System.out.print("Enter Fee Structure : ");
-            // int feeStructure = input.nextInt();
-
-            // System.out.print("Enter maximum student : ");
-            // int maximumStudent = input.nextInt();
-
-            // System.out.println("Scholarship Facilities : ");
-            // boolean scholarshipFacilities = input.nextBoolean();
-
-            // Course c = new Course("2121212", courseName, programme, courseDuration, dd,
-            // ee, true, feeStructure,
-            // maximumStudent, MediumOfStudy.FOUNDATION);
-            // Course science = new Course("ODN3829", "Bachelor of Science (Hons.)
-            // Intelligent Robotics",
-            // new String[] { "Data Structures", "Algorithms", "Programming Languages" },
-            // 120,
-            // new String[] { "Masters in Computer Science",
-            // "MBA in IT" },
-            // new String[] { "hello" },
-            // true, 14, 120, MediumOfStudy.FOUNDATION);
-
-            Course c = new Course(courseName);
-            Course.addCourse(c);
-
-            List<Course> allCourses = Course.getAllCourses();
-
-            for (Course course : allCourses) {
-                System.out.println(course.getCourseName());
-            }
-            System.out.println("\nCourse " + c.getCourseName() + " has been created successfdeully.");
-
-            System.out.print("\nPress 0 to return : ");
-            int selection = input.nextInt();
-            if (selection == 0)
-                manageCourse(c, loggedInUser);
-        } catch (
-
-        NumberFormatException e) {
-            e.printStackTrace();
         }
     }
 
