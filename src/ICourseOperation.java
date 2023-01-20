5eca9865bda7d5ca179775d3c80248b43735c38a
Nik Uzair <nikuzairsc@gmail.com>
2023-01-20 10:47:21 +0800

    Update ICourseOperation.java
    - Revise code.

--- src/ICourseOperation.java
+++ src/ICourseOperation.java
@@ -2,7 +2,7 @@ interface ICourseOperation {
 
     void manageCourse(Course selectedCourse, User loggedInUser);
 
-    void addCourse(Course selectedCourse, User loggedInUser);
+    // void addCourse(Course selectedCourse, User loggedInUser);
 
     void unregisteredFromCourse(Course course, User loggedInUser);
 }
