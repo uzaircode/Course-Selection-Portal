bb289572879368d1c326bfa4b09e94dbcae6981e
Nik Uzair <nikuzairsc@gmail.com>
2023-01-21 08:45:05 +0800

    Update main.java
    - Revise code to make it more user friendly.
    - Fix bug select user input not working for looping.

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,74 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Registration res = new Registration();
         try (Scanner input = new Scanner(System.in)) {
-            System.out.print("\033[H\033[2J");
-            System.out.println("===== MULTIMEDIA UNVERSITY =====");
-            System.out.println("\n(1) STUDENT PORTAL");
-            System.out.println("(2) ADMIN PORTAL");
-            System.out.println("(3) MMU PORTAL");
+            while (true) {
+                DashboardDisplayStrategy strategy = null;
 
-            System.out.print("\nChoose 1 : ");
-            int selection = input.nextInt();
-            if (selection == 1) {
-                res.displayStudentPortal();
-            } else if (selection == 2) {
-                res.displayAdminPortal();
-            } else if (selection == 3) {
-                res.displayMMUPortal();
+                System.out.print("\033[H\033[2J");
+                System.out.println("===== WELCOME TO MMU PORTAL =====");
+                System.out.println("\n(1) VIEW ALL COURSES ");
+                System.out.println("(2) VIEW COURSE BRIEFING");
+                System.out.println("(3) STUDENT PORTAL");
+                System.out.println("(4) ADMIN PORTAL");
+                System.out.println("(5) EXIT PORTAL");
+
+                System.out.print("\nChoose 1 : ");
+                int selection = input.nextInt();
+                switch (selection) {
+                    case 1:
+                        viewAllCourses();
+                        break;
+                    case 2:
+                        viewCourseBriefing();
+                        break;
+                    case 3:
+                        strategy = new StudentDashboardDisplayStrategy();
+                        break;
+                    case 4:
+                        strategy = new AdminDashboardDisplayStrategy();
+                        break;
+                    case 5:
+                        return;
+                }
+                if (strategy != null) {
+                    strategy.display();
+                }
             }
         }
+    }
+
+    public static void viewCourseBriefing() {
+        System.out.print("\033[H\033[2J");
+        System.out.println("===== COURSE BRIEFING VIDEO =====");
+        System.out.println("\nDisplay course briefing...");
+
+        System.out.print("\nPress 0 to return : ");
+        int selection = input.nextInt();
+        if (selection == 0)
+            return;
+    }
+
+    public static void viewAllCourses() {
+        System.out.print("\033[H\033[2J");
+        System.out.println("===== BROWSE THE PROGRAMMES =====\n");
+        Course courseList = new Course();
+        List<Course> courses = courseList.getAllCourses();
+
+        int i = 1;
+        for (Course course : courses) {
+            System.out.println("(" + i + ") " + course.getCourseName());
+            i++;
+        }
+
+        System.out.print("\nPress 0 to return : ");
+        int selection = input.nextInt();
+        if (selection == 0)
+            return;
 
     }
 }
