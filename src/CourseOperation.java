43b5de58503efbf8504a70a3f09479d566c9d7ad
Nik Uzair <nikuzairsc@gmail.com>
2023-01-21 08:53:45 +0800

    Delete CourseOperation.java

--- src/CourseOperation.java
+++
@@ -1,9 +0,0 @@
-abstract class CourseOperation {
-    // template method
-    public void execute(Course selectedCourse, User loggedInUser) {
-        addCourse(selectedCourse, loggedInUser);
-    }
-
-    // abstract methods
-    public abstract void addCourse(Course selectedCourse, User loggedInUser);
-}
